4762a0eb1d352d969d43ea8d822147584b502557
Antonio Ramirez <antonioramirez9614@gmail.com>
2024-07-28 09:22:48 -0500

    finished feature!

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,39 @@ import './App.css'
 import { puppyList } from './data.js'
 import { useState } from 'react'
 
-function App() {
-  const [puppies, setPuppies] = useState(puppyList)
 
-  console.log(puppies)
+function App() {
+  const [puppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+
+  const clickPup = (puppy) => {
+    setFeatPupId(puppy.id)
+  }
+
   return (
     <>
-    <div>
+    <h1>Puppies!</h1>
+    <section className="App">
       {puppies.map(puppy => {
-        return <p key={puppy.id}>{puppy.name}</p>
+        return <p onClick={()=>clickPup(puppy)}
+        key={puppy.id}
+        >
+        {puppy.name}
+        </p>
       })}
-    </div>
+      {/* conditional rendering for FeatPup */}
+      { featPupId &&
+      <div>
+      <h2>Name: {featuredPup.name}</h2>
+         <ul>
+          <li> age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+          </ul>
+      </div>
+      }
+    </section>
     </>
   )
 }
